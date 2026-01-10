b19ddf52e087ee13aa6f45021218a5838a1ac10e
edy <edycoleee@gmail.com>
2026-01-10 07:38:03 +0700

    coba

---
+++ config.py
@@ -0,0 +1,32 @@
+#config.py
+import os
+
+class Config:
+    # --- PACS & ROUTER CONFIG ---
+    DCM4CHEE_URL = os.getenv("DCM4CHEE_URL", "http://192.10.10.23:8081/dcm4chee-arc/aets/DCM4CHEE")
+    
+    ROUTER_IP = os.getenv("ROUTER_IP", "192.10.10.51")
+    ROUTER_PORT = os.getenv("ROUTER_PORT", "11112")
+    ROUTER_AET = os.getenv("ROUTER_AET", "DCMROUTER")
+    
+    # --- SATUSEHAT CONFIG ---
+    # Menggunakan environment variable agar credential tidak hardcoded di production
+    SS_AUTH_URL = os.getenv("SS_AUTH_URL", "https://api-satusehat.kemkes.go.id/oauth2/v1")
+    SS_BASE_URL = os.getenv("SS_BASE_URL", "https://api-satusehat.kemkes.go.id/fhir-r4/v1")
+    SS_ORG_ID = os.getenv("SS_ORG_ID", "100025702")
+    SS_CLIENT_ID = os.getenv("SS_CLIENT_ID", "Gzn7YjXv0tQrNIlzgFliVAiBBkLLgVLEYsuXqmNVnwfapAxD")
+    SS_CLIENT_SECRET = os.getenv("SS_CLIENT_SECRET", "fbPy8SDIkcrx1V7OaH1mHfx0xebHwBJBL4Fw8gW0Gi6MjIFrt9vWgW7NOIVNibGt")
+
+    # --- SYSTEM CONFIG ---
+    LOG_FILE = os.getenv("LOG_FILE", "app_dicom.log")
+    TEMP_DIR = os.getenv("TEMP_DIR", "/tmp/dicom_gateway_tmp")
+
+    @classmethod
+    def init_app(cls):
+        """Memastikan folder temporary tersedia saat aplikasi start"""
+        if not os.path.exists(cls.TEMP_DIR):
+            try:
+                os.makedirs(cls.TEMP_DIR, exist_ok=True)
+                print(f"[*] Temporary directory created at: {cls.TEMP_DIR}")
+            except Exception as e:
+                print(f"[!] Failed to create temp directory: {e}")
